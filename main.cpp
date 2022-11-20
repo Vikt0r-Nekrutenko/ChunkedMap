5677f59296bb5fd6aa942cbb561594a7b4b7ed9f
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-20 05:05:55 +0200

    TMP

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <thread>
 #include "window.hpp"
 
 using namespace std::chrono;
@@ -52,11 +53,13 @@ struct chunkscontroller
 {
     struct chunkrecord
     {
-        stf::Vec2d pos;
-        chunk *ch;
+        stf::Vec2d pos {0,0};
+        chunk *ch {nullptr};
     };
 
     std::vector<chunkrecord> mChunks;
+    std::vector<chunkrecord> mCache;
+    chunkrecord mTmpCache;
     chunkrecord empty = {{0,0}, new chunk('.')};
     const stf::Vec2d Size{0,0};
 
@@ -71,21 +74,62 @@ struct chunkscontroller
     const int bottomLim = bottomLimEnd - mRightBottom.y;
     const int bottomLimBegin = bottomLim - mLeftTop.y;
 
+    const int cacheW;
+    const int cacheH;
 
     chunkscontroller(int w, int h, const stf::Vec2d &leftTop, const stf::Vec2d &rightBottom)
         : Size{w,h},
           mLeftTop{leftTop},
-          mRightBottom{rightBottom}
+          mRightBottom{rightBottom},
+        cacheW {(leftTop.x + rightBottom.x) / chunk::W},
+        cacheH {(leftTop.y + rightBottom.y) / chunk::H}
     {
-        mChunks.resize(w * h, {{0,0},nullptr});
+//        mChunks.resize(w * h, {{0,0},nullptr});
+
+        mCache.resize(cacheW * cacheH);
+        FILE *file = std::fopen("chunks.dat", "wb");
+        for(int i = 0; i < h; ++i) {
+            for(int j = 0; j < w; ++j) {
+                stf::Vec2d chunkPos = stf::Vec2d(j,i) * stf::Vec2d(chunk::W, chunk::H);
+                uint8_t isNull = true;
+                uint8_t chunkmem[sizeof(chunk)];
+                memset(chunkmem, 0, sizeof(chunkmem));
+
+                std::fwrite(&isNull, sizeof(uint8_t), 1, file);
+                std::fwrite(&chunkPos.x, sizeof(int), 1, file);
+                std::fwrite(&chunkPos.y, sizeof(int), 1, file);
+                std::fwrite(chunkmem, sizeof(chunkmem), 1, file);
+            }
+        }
+        std::fclose(file);
     }
 
     void show(stf::Renderer &renderer, const stf::Vec2d &posOnScreen) const
     {
-        for(auto &chr : mChunks) {
+        for(auto &chr : mCache) {
             if(chr.ch != nullptr)
                 chr.ch->show(renderer, chr.pos + posOnScreen);
         }
+    }
+
+    size_t loadedChunks() const
+    {
+        size_t n = 0;
+        for(auto ch : mCache) {
+            if(ch.ch != nullptr)
+                ++n;
+        }
+        return n;
+    }
+
+    size_t memUsage() const
+    {
+        size_t n = 0;
+        for(auto ch : mCache) {
+            if(ch.ch != nullptr)
+                n += sizeof(chunk);
+        }
+        return n;
     }
 
     void show(stf::Renderer &renderer, const stf::Vec2d &posOnScreen, const stf::Vec2d &camera)
@@ -111,11 +155,45 @@ struct chunkscontroller
             p2.y = bottomLimEnd;
         }
 
-        for(int y = p1.y, sy = posOnScreen.y; y < p2.y; ++y, ++sy) {
-            for(int x = p1.x, sx = posOnScreen.x; x < p2.x; ++x, ++sx) {
+//        for(int y = p1.y, sy = posOnScreen.y; y < p2.y; ++y, ++sy) {
+//            for(int x = p1.x, sx = posOnScreen.x; x < p2.x; ++x, ++sx) {
+//                (*this)[{x,y}].ch->show(renderer, {sx,sy}, {x,y});
+//            }
+//        }
+        for(int y = 0, sy = posOnScreen.y; y < chunk::H; ++y, ++sy) {
+            for(int x = 0, sx = posOnScreen.x; x < chunk::W; ++x, ++sx) {
                 (*this)[{x,y}].ch->show(renderer, {sx,sy}, {x,y});
             }
         }
+    }
+
+    uint8_t& preload(const stf::Vec2d &pos)
+    {
+        FILE *file = std::fopen("chunks.dat", "r+b");
+        size_t seek = sizeof(uint8_t) + sizeof(stf::Vec2d) + sizeof(chunk);
+
+//        for(int y = 0; y < cacheH; ++y) {
+//            for(int x = 0; x < cacheW; ++x) {
+//                size_t i = cacheW * y + x;
+                size_t j = Size.x * pos.y + pos.x;
+                fseek(file, j * seek, SEEK_SET);
+                uint8_t isNull = 1;
+                std::fread(&isNull, sizeof(uint8_t), 1, file);
+                std::fread(&mTmpCache.pos, sizeof(stf::Vec2d), 1, file);
+//                if(isNull) {
+                    mTmpCache.ch = new chunk();
+                    isNull = 0;
+                    fseek(file, j * seek, SEEK_SET);
+                    std::fwrite(&isNull, sizeof(uint8_t), 1, file);
+                    std::fseek(file, sizeof(stf::Vec2d), SEEK_CUR);
+                    std::fwrite(mTmpCache.ch, sizeof(chunk), 1, file);
+//                } else {
+//                  std::fread(mTmpCache.ch, sizeof(chunk), 1, file);
+//                }
+//            }
+//        }
+        std::fclose(file);
+        return (*empty.ch)[pos];
     }
 
     uint8_t& operator ()(const stf::Vec2d &pos)
@@ -125,39 +203,51 @@ struct chunkscontroller
     chunkrecord& operator [](const stf::Vec2d &pos)
     {
         stf::Vec2d chunkBeginPos = pos / stf::Vec2d(chunk::W, chunk::H);
+        stf::Vec2d chunkInCachePos = {pos.x % cacheW, pos.y % cacheH};
+//        if(pos.x < 0 || pos.y < 0 || pos.x > Size.x * chunk::W - 1 || pos.y > Size.y * chunk::H - 1)
         if(pos.x < 0 || pos.y < 0 || pos.x > Size.x * chunk::W - 1 || pos.y > Size.y * chunk::H - 1)
 //            throw std::out_of_range(std::to_string(Size.x * pos.y + pos.x));
             return empty;
+        preload(chunkBeginPos);
+//        if(mChunks[Size.x * chunkBeginPos.y + chunkBeginPos.x].ch != nullptr)
+//            return mChunks[Size.x *  chunkBeginPos.y + chunkBeginPos.x];
 
-        if(mChunks[Size.x * chunkBeginPos.y + chunkBeginPos.x].ch != nullptr)
-            return mChunks[Size.x *  chunkBeginPos.y + chunkBeginPos.x];
+//        mChunks[Size.x *  chunkBeginPos.y + chunkBeginPos.x] = {chunkBeginPos, new chunk()};
+//        return mChunks[Size.x *  chunkBeginPos.y + chunkBeginPos.x];
+//        if(mCache[Size.x * chunkBeginPos.y + chunkBeginPos.x].ch != nullptr)
+//            return mCache[Size.x *  chunkBeginPos.y + chunkBeginPos.x];
 
-        mChunks[Size.x *  chunkBeginPos.y + chunkBeginPos.x] = {chunkBeginPos, new chunk()};
-        return mChunks[Size.x *  chunkBeginPos.y + chunkBeginPos.x];
+//        mCache[Size.x *  chunkBeginPos.y + chunkBeginPos.x] = {chunkBeginPos, new chunk()};
+//        return mCache.at(cacheW * chunkInCachePos.y + chunkInCachePos.x).ch == nullptr ? empty : mCache[cacheW *  chunkBeginPos.y + chunkBeginPos.x];
+        return mTmpCache.ch == nullptr ? empty : mTmpCache;
     }
 };
 
 class Game : public stf::Window
 {
     bool isContinue = true;
-    chunkscontroller chc{3,3, {2,2}, {3,3}};
-    stf::Vec2d player = {4,4};
-    stf::Vec2d lt {2,2};
-    stf::Vec2d rb = {3,3};
+    chunkscontroller chc{30,30, {5,6}, {5,6}};
+    stf::Vec2d player = {0,0};
 
 public:
-    Game() { enableLog(); stf::Renderer::log.setX(20); }
+
+    Game()
+    {
+        enableLog(); stf::Renderer::log.setX(40); stf::Renderer::log.setHeight(30);
+    }
 
     bool onUpdate(const float dt) override
     {
-        chc(player) = 'O';
         chc.show(renderer, {0,2}, player);
+        stf::Renderer::log<<stf::endl<<" Chunks: "<<(int)chc.loadedChunks()<<" mem: "<<(float)chc.memUsage()/1'000'000.f<<"MB"
+                            ;
+//        chc(player) = 'O';
         return isContinue;
     }
 
     void keyEvents(const int key) override
     {
-        chc(player) = chc[player].ch->sym;
+//        chc(player);// = chc[player].ch->sym;
         switch (key) {
         case 'w':player.y--;break;
         case 's':player.y++;break;
@@ -176,5 +266,9 @@ public:
 
 int main()
 {
-    return Game().run();
+//    return Game().run(30);
+    chunkscontroller chc{30,30, {5,6}, {5,6}};
+    std::cout << chc.mTmpCache.pos.x << " " << chc.mTmpCache.pos.x << std::endl;
+    std::cout << chc[{12,0}].pos.x << " " << chc[{12,0}].pos.x << std::endl;
+    std::cout << chc[{12,0}].ch->sym << std::endl;
 }
