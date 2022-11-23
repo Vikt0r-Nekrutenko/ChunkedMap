92f7ce58f32c59cce361b47640694bacc003cc9b
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-23 06:12:50 +0200

    050 WORK ON STF

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <chrono>
 #include "window.hpp"
-#include "chunkedmap.hpp"
+#include "sdb/chunkedmap.hpp"
+#include "sdb/chunk.hpp"
 
-struct Cell : ICell
+struct Cell : stf::sdb::ICell
 {
     uint8_t v;
 
@@ -31,14 +33,15 @@ struct Cell : ICell
     }
 };
 
-struct Chunk : public IChunk
+struct Chunk : public stf::sdb::IChunk
 {
     Chunk()
         : IChunk{{8,8}}
     {
         mArray.resize(64);
+        uint8_t s = 'a'+rand()%('z'-'a');
         for(auto &c : mArray)
-            c = new Cell{(uint8_t)('a'+rand()%('z'-'a'))};
+            c = new Cell{(uint8_t)(s)};
     }
 
 };
@@ -46,42 +49,120 @@ struct Chunk : public IChunk
 
 class Game : public stf::Window
 {
+    int w = 3000, h = w;
     bool isContinue = true;
-    ChunkedMapT<Chunk> chc = ChunkedMapT<Chunk>{8,8};
-    stf::Vec2d player = {4,4};
+    stf::sdb::ChunkedMapT<Chunk> chc = stf::sdb::ChunkedMapT<Chunk>{w,h};
+    stf::Vec2d player = {14,19};
 
 public:
 
-    Game()
-    {
-        enableLog(); stf::Renderer::log.setX(40); stf::Renderer::log.setHeight(30);
-    }
+    float sec = 0;
+    float chunkReloadCounter = 0;
+    int delt = 0;
+    int avarageTimeToLoadChunk = 0;
+    bool isOnPause = 0;
+
+    stf::Vec2f vel = {0,0};
+    float px = 13.f, py = 19.f;
+
     bool onUpdate(const float dt) override
     {
-        for(int j = 0, y = player.y-4; j < 9; ++j, ++y) {
-            for(int i = 0, x = player.x-4; i < 9; ++i, ++x) {
+        if(sec > 200.f)
+        {
+            avarageTimeToLoadChunk = delt / chunkReloadCounter;
+            delt = 0;
+            chunkReloadCounter = 0;
+            sec = 0;
+        }
+        sec += dt;
+
+        if(isOnPause)
+            return isContinue;
+
+        using namespace std::chrono;
+        for(int j = 2, y = player.y-13; j < 29; ++j, ++y) {
+            for(int i = 1, x = player.x-19; i < 38; ++i, ++x) {
                 auto ch = chc[stf::Vec2d{x, y}];
                 if(ch != nullptr) {
-                    Cell *c = static_cast<Cell*>(chc.at({x,y}));
+                    auto t1 = high_resolution_clock::now();
+
+                    Cell *c = static_cast<Cell*>(ch->at({x,y}));
                     renderer.drawPixel(stf::Vec2d{i, j}, c->view());
+
+                    auto t2 = high_resolution_clock::now();
+                    delt += duration_cast<microseconds>(t2 - t1).count();
+                    ++chunkReloadCounter;
                 } else
                     renderer.drawPixel(stf::Vec2d{i, j}, '.');
             }
         }
-        renderer.drawPixel(player - (player - 4), 'I');
-        renderer.draw({0, 10}, "%d %d", player.x,player.y);
-        stf::Renderer::log<<stf::endl<<"Chunks: "<<(int)chc.cacheSize()<<" mem: "<<(float)chc.memUsage()/1'000.f<<"KB";
+
+        chc.cache().update(dt);
+
+        renderer.drawPixel(player - (player - stf::Vec2d{19,13}), 'I', stf::ColorTable::Red);
+        renderer.draw({40, 2}, "POS: X[%d]:Y[%d]", player.x, player.y);
+        renderer.draw({40, 3}, "VEL: X[%f]:Y[%f]", vel.x, vel.y);
+
+
+        renderer.draw({40,5}, "New/Del OP : [%d] [%d]", (int)chc.cache().mNewOp, (int)chc.cache().mDelOp);
+        renderer.draw({40,6}, "Inp/Outp   : %d", (int)chc.cache().IOCount());
+        renderer.draw({40,7}, "Chunks     : %d", (int)chc.cache().cacheSize());
+        renderer.draw({40,8}, "Memory     : %fkb", (float)chc.cache().memUsage()/1'000.f);
+
+        renderer.draw({40,9}, "Load T(average) : %dmicS", avarageTimeToLoadChunk);
+        renderer.draw({40,10}, "Load T          : %dmicS", chc.cache().mLoadT);
+
+        renderer.draw({40, 12}, "Misses : %d", (int)chc.cache().cacheMisses());
+        renderer.draw({40, 13}, "Hits   : %d", (int)chc.cache().cacheHits());
+        renderer.draw({40, 14}, "M&S    : %d", (int)chc.cache().cacheCalls());
+        renderer.draw({40, 15}, "M/S    : %d", (int)chc.cache().cacheHits() / (int)chc.cache().cacheMisses());
+
+        int y = 17;
+        int x = 40;
+        for(auto &chunk : chc.cache().mL1) {
+            renderer.draw({x, y++}, "%d (%d) [%d]=[%d]---%d:%d",
+                          y - 17,
+                          chunk.mIsActive,
+                          chunk.mLifeTime,
+                          chunk.mHits - chunk.mCalls,
+                          chunk.mChunkRec.mPos.x,
+                          chunk.mChunkRec.mPos.y);
+        }
+
+
+        if(player.x < 1) {
+            vel.x = -vel.x;
+            px = 1;
+        }
+        if(player.x > w * Chunk().size().x - 1) {
+            vel.x = -vel.x;
+            px = (w * Chunk().size().x) - 2;
+        }
+        if(player.y < 1){
+            vel.y = -vel.y;
+            py = 1;
+        }
+        if(player.y > w * Chunk().size().y - 1) {
+            vel.y = -vel.y;
+            py = (w * Chunk().size().y) - 2;
+        }
+
+        px += vel.x * dt;
+        py += vel.y * dt;
+
+        player = { px, py };
+
         return isContinue;
     }
 
     void keyEvents(const int key) override
     {
         switch (key) {
-        case 'w':player.y--; break;
-        case 's':player.y++; break;
-        case 'a':player.x--; break;
-        case 'd':player.x++; break;
-        case ' ':chc.put(player, new Cell('W')); break;
+        case 'w':vel.y -= 0.001f; break;
+        case 's':vel.y += 0.001f; break;
+        case 'a':vel.x -= 0.001f; break;
+        case 'd':vel.x += 0.001f; break;
+        case ' ': isOnPause ^= 1; break;
         case 'q':isContinue = false;break;
         default:break;
         }
@@ -92,8 +173,8 @@ public:
 
     }
 };
+
 int main()
 {
     return Game().run();
-//    std::cout << Chunk({8,8}).sizeOfSelf();
 }
